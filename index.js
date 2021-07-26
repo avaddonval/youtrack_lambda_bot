e905b883e8dde9e872a8e208d63d37545b8706c1
Валерий Забродин <avaddonval@gmail.com>
2021-07-26 09:14:23 +0600

    check is week end and not notify

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const tg = require('./telegram')
 const yt = require('./youtrack')
 exports.handler =  async function(event, context) {
-  let items = await yt.getWorkItems(event?event.day:'yesterday');
+  if(checkIsWeekEnd()) return true
+  let items = await yt.getWorkItems(event?event.day:'today');
   let res = await tg.sendWorkItems(items).catch(err=>{console.log(err)});
   console.log(res)
   return res;
 }
 //just for local tests
-/* ;(async () => {
+ /* ;(async () => {
   try {
     await exports.handler()
   }catch(err){
     console.log(err)
   }
-})(); */
+})();  */
+function checkIsWeekEnd(date = new Date){
+  let dayOfWeek = date.getDay();
+  return (dayOfWeek === 6) || (dayOfWeek  === 0); 
+}
